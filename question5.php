afa93c46d447116abf1d6737190564ab39d28ddd
Sabiriin Said Ali <sabirinsaciid76@gmail.com>
2026-09-25 12:40:37 -0800

    Create question5.php

---
+++ question5.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <title>Document</title>
+</head>
+<body>
+    <?php
+
+$num = 12345;
+$reverse = 0;
+
+while ($num > 0) {
+
+    $digit = $num % 10;
+
+    $reverse = ($reverse * 10) + $digit;
+
+    $num = (int)($num / 10);
+}
+
+echo "Reverse = " . $reverse;
+
+?>
+</body>
+</html>
